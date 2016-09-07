7619b6b557fd4eee99e1b83e6c5e6b19981df0dc
Kevin <kgresmer@gmail.com>
2016-09-06 21:48:21 -0700

    Added images of each side of the dice and the roll dice method with a roll button.

--- app/farkle/farkle.js
+++ app/farkle/farkle.js
@@ -8,6 +8,41 @@ angular.module('mainPage.farkle', ['ngRoute'])
             controller: 'FarkleController'
         });
     }])
-    .controller('FarkleController', [function () {
+    .controller('FarkleController', ['$scope', function ($scope) {
+
+        var getDiceValue = function (min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            var value = Math.floor(Math.random() * (max - min)) + min;
+            switch (value) {
+                case 1:
+                    return 'dice-one';
+                    break;
+                case 2:
+                    return 'dice-two';
+                    break;
+                case 3:
+                    return 'dice-three';
+                    break;
+                case 4:
+                    return 'dice-four';
+                    break;
+                case 5:
+                    return 'dice-five';
+                    break;
+                case 6:
+                    return 'dice-six';
+                    break;
+            }
+        };
+        
+        $scope.rollDice = function() {
+            $scope.dice_one = getDiceValue(1, 6);
+            $scope.dice_two = getDiceValue(1, 6);
+            $scope.dice_three = getDiceValue(1, 6);
+            $scope.dice_four = getDiceValue(1, 6);
+            $scope.dice_five = getDiceValue(1, 6);
+            $scope.dice_six = getDiceValue(1, 6);
+        }
 
     }]);
