b212dcae61e4a4b456137f7ab4fb50147931222f
Kevin <kgresmer@gmail.com>
2016-09-06 22:31:19 -0700

    Dice are now objects held in an array.
    Get dice value method is started.
    
    It does a poor job of keeping score.

--- app/farkle/farkle.js
+++ app/farkle/farkle.js
@@ -10,39 +10,81 @@ angular.module('mainPage.farkle', ['ngRoute'])
     }])
     .controller('FarkleController', ['$scope', function ($scope) {
 
+        $scope.tempScore = 0;
+        $scope.totalScore = 0;
+
         var getDiceValue = function (min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
             var value = Math.floor(Math.random() * (max - min)) + min;
             switch (value) {
                 case 1:
-                    return 'dice-one';
+                    return {
+                        image: 'dice-one',
+                        value: 1
+                    };
                     break;
                 case 2:
-                    return 'dice-two';
+                    return {
+                        image: 'dice-two',
+                        value: 2
+                    };
                     break;
                 case 3:
-                    return 'dice-three';
+                    return {
+                        image: 'dice-three',
+                        value: 3
+                    };
                     break;
                 case 4:
-                    return 'dice-four';
+                    return {
+                        image: 'dice-four',
+                        value: 4
+                    };
                     break;
                 case 5:
-                    return 'dice-five';
+                    return {
+                        image: 'dice-five',
+                        value: 5
+                    };
                     break;
                 case 6:
-                    return 'dice-six';
+                    return {
+                        image: 'dice-six',
+                        value: 6
+                    };
                     break;
             }
         };
         
         $scope.rollDice = function() {
-            $scope.dice_one = getDiceValue(1, 6);
-            $scope.dice_two = getDiceValue(1, 6);
-            $scope.dice_three = getDiceValue(1, 6);
-            $scope.dice_four = getDiceValue(1, 6);
-            $scope.dice_five = getDiceValue(1, 6);
-            $scope.dice_six = getDiceValue(1, 6);
+            $scope.dice = [];
+            for (var i = 1; i < 7; i++) {
+                $scope.dice[i] = getDiceValue(1, 6);
+            }
+            $scope.getDiceValue();
+        };
+
+        $scope.getDiceValue = function() {
+            var totalValue = 0;
+            for (var i = 1; i < $scope.dice.length + 1; i++) {
+                if (!$scope.dice[i]) {
+                    continue;
+                }
+                //check for
+                // 6 of a kind
+                // straight
+                // two triples
+                // three doubles
+
+                if ($scope.dice[i].value === 1) {
+                    totalValue += 100;
+                } else if ($scope.dice[i].value === 5) {
+                    totalValue += 50;
+                }
+            }
+            $scope.tempScore = totalValue;
+            $scope.totalScore += totalValue;
         }
 
     }]);
