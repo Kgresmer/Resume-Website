949188305e459d9966d5be6478686af02fc0f8db
kgresmer <Jasong55>
2016-09-14 15:52:17 -0700

    begin adding add player directive.

---
+++ app/farkle/add_player_directive.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chq-keving on 9/14/2016.
+ */
+
+angular.module('mainPage.farkle', [])
+    .directive('addPlayer')
+
+http://weblogs.asp.net/dwahlin/creating-custom-angularjs-directives-part-i-the-fundamentals
